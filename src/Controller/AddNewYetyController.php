03b6285c8809310869884562648d62e04993a92a
lesak <tripsimon3@gmail.com>
2025-07-07 11:04:03 +0200

    pridavani yetyho

--- src/Controller/AddNewYetyController.php
+++ src/Controller/AddNewYetyController.php
@@ -1,22 +1,46 @@
 <?php
-
 namespace App\Controller;
 
+use App\Entity\Yeti;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Connection;
 use App\Form\YetiForm;
 
 final class AddNewYetyController extends AbstractController
 {
     #[Route('/addNewYeti', name: 'app_add_new_yeti')]
-    public function index(): Response
+    public function index(Request $request, Connection $connection): Response
     {
-        $form = $this->createForm(YetiForm::class);
+        $newYeti = new Yeti(); 
+
+        $form = $this->createForm(YetiForm::class, $newYeti);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            $connection->insert('yeti', [
+                'name' => $newYeti->getName(),
+                'age' => $newYeti->getAge(),
+                'gender' => $newYeti->getGender()?->value,
+                'weight' => $newYeti->getWeight(),
+                'height' => $newYeti->getHeight(),
+                'place_of_stay' => $newYeti->getPlaceOfStay(),
+                'current_rating' => 0
+            ]);
+
+            $lastInsertId = $connection->lastInsertId();
+
+            $this->addFlash('success', 'Yeti byl úspěšně přidán!');
+            return $this->redirectToRoute('yeti_detail',[
+                'id' => $lastInsertId,
+            ]);
+        }
 
         return $this->render('add_new_yeti/index.html.twig', [
-            'controller_name' => 'AddNewYetyController',
-            'form'=> $form->createView(),
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/YetiForm.php
+++ src/Form/YetiForm.php
@@ -5,9 +5,13 @@ namespace App\Form;
 use App\Entity\Yeti;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;;
 use App\Enum\Gender;
 
 class YetiForm extends AbstractType
@@ -15,14 +19,37 @@ class YetiForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', )
-            ->add('weight')
-            ->add('age')
-            ->add('height')
+            ->add('name', TextType::class, [
+    'label' => 'Jméno',
+])
+            ->add('weight', IntegerType::class, [
+                'label' => 'Váha',
+                'constraints' => [
+                    new Positive([
+                        'message' => 'Hodnota musí být kladné číslo.',
+                    ]),
+                ],
+            ])
+            ->add('age', IntegerType::class, [
+                'label' => 'Věk',
+                'constraints' => [
+                    new Positive([
+                        'message' => 'Hodnota musí být kladné číslo.',
+                    ]),
+                ],
+            ])
+            ->add('height', IntegerType::class, [
+                'label' => 'Výška',
+                'constraints' => [
+                    new Positive([
+                        'message' => 'Hodnota musí být kladné číslo.',
+                    ]),
+                ],
+            ])
             ->add('placeOfStay')
-            ->add('gender', EnumType::class,['class' => Gender::class])
+            ->add('gender', EnumType::class, ['class' => Gender::class])
             ->add('currentRating')
-            ->add('submit', SubmitType::class, ['label'=> 'Vytvořit'])
+            ->add('submit', SubmitType::class, ['label' => 'Vytvořit'])
         ;
     }
 
